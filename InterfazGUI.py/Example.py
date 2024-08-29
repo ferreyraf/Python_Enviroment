c77e1e051f324ee584c79cc317ed832220da1e3b
Fer F <fernandoferreyra11@gmail.com>
2024-08-29 20:30:27 -0300

    Second

---
+++ InterfazGUI.py/Example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+from tkinter import ttk
+
+
+def Aplicacion():
+    pass
+
+
+
+
+def main():
+    mi_app = Aplicacion()
+    return 0
+
+if __name__ == '__main__':
+    main()
